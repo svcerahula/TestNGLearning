3332c637bda4de6f1343b4b84da45a9582a244ab
817870 <svce.rahula@gmail.com>
2020-05-09 22:49:59 +0530

    TestNG features Initial learning

---
+++ src/test/java/BasicsTestNG/TestNGDemo1.java
@@ -0,0 +1,42 @@
+package BasicsTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestNGDemo1 {
+
+    WebDriver wd1 = null;
+    @BeforeMethod
+    public void setupDriver() {
+        System.setProperty("webdriver.chrome.driver","C:\\Rahula Space\\Drivers\\chromedriver_win32_ver79\\chromedriver.exe");
+        ChromeOptions option1 = new ChromeOptions();
+        Map<String,Object> prefs = new HashMap<>();
+        //setting this value for ignoring /block notification in the browser
+        prefs.put("profile.default_content_setting_values.notifications",2);
+        option1.setExperimentalOption("prefs",prefs);
+        wd1 = new ChromeDriver(option1);
+    }
+
+    @Test
+    public void goToAmazonHomePage() {
+        wd1.get("http://www.amazon.in");
+        System.out.println("Inside Amazon HomePage India");
+    }
+
+    @Test
+    public void goToGoogleHomePage() {
+        wd1.get("http://www.google.in");
+        System.out.println("Inside Google HomePage India");
+    }
+    @AfterMethod
+    public void closeSetup() {
+        wd1.close();
+    }
+}
---
+++ src/test/java/BasicsTestNG/TestNGDemo2.java
@@ -0,0 +1,38 @@
+package BasicsTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestNGDemo2 {
+    WebDriver wd1 = null;
+    @BeforeMethod
+    public void setupDriver() {
+        System.setProperty("webdriver.chrome.driver","C:\\Rahula Space\\Drivers\\chromedriver_win32_ver79\\chromedriver.exe");
+        ChromeOptions option1 = new ChromeOptions();
+        Map<String,Object> prefs = new HashMap<>();
+        //setting this value for ignoring /block notification in the browser
+        prefs.put("profile.default_content_setting_values.notifications",2);
+        option1.setExperimentalOption("prefs",prefs);
+        wd1 = new ChromeDriver(option1);
+    }
+
+    @Test
+    public void goToFlipKartHomePage() {
+        wd1.get("https://www.flipkart.com/");
+        System.out.println("Inside FlipKart Homepage");
+        wd1.close();
+    }
+
+    @Test
+    public void goToFacebookHomePage() {
+        wd1.get("https://www.facebook.com/");
+        System.out.println("Inside Facebook Homepage");
+        wd1.close();
+    }
+}
